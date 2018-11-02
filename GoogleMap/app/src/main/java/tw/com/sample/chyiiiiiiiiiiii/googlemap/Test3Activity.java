27fbd7a7470d0b3a79865042c81a5785da94d0f7
Yii <ab20803@gmail.com>
2018-11-02 18:57:13 +0800

    update 'GoogleMap' project

--- GoogleMap/app/src/main/java/tw/com/sample/chyiiiiiiiiiiii/googlemap/Test3Activity.java
+++ GoogleMap/app/src/main/java/tw/com/sample/chyiiiiiiiiiiii/googlemap/Test3Activity.java
@@ -1,10 +1,12 @@
 package tw.com.sample.chyiiiiiiiiiiii.googlemap;
 
+import com.google.android.gms.maps.model.MapStyleOptions;
+
 public class Test3Activity extends MapActivity {
 
     @Override
     protected void onMapReady() {
-
+        getMap().setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.style2));
     }
 
 }
